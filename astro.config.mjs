3b1f92ab549f0849d806774462011c4cdc0ab874
Cesar Luis Aybar Camacho <aybar1994@gmail.com>
2024-09-29 17:10:29 +0200

    up

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
+
 
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',  
+  output: 'server',
+  adapter: vercel(),
   site: 'https://tacofoundation.github.io',
   outDir: 'dist',
 });

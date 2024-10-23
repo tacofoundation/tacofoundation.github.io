a05c056d0a4a540e3802ae3aa07ed008d5ee1b39
ingrid1802 <ingridaybar841@gmail.com>
2024-10-22 22:42:35 -0500

    actualizaciones

---
+++ src/config/nav.js
@@ -0,0 +1,8 @@
+export const navItems = [
+  { name: 'Home', href: '/' },
+  { name: 'Overview', href: '/overview' },
+  { name: 'Contributing', href: '/contributing' },
+  { name: 'Dataset', href: '/dataset' },
+  // { name: 'Sample Datasets', href: '/sample-datasets' }, 
+  // { name: 'Spec', href: '/spec' },
+];
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,13 +1,30 @@
 import { defineCollection, z } from "astro:content";
-// z -> zod schema/tipo de datos
 
+// Definición de la colección para los datasets
 const dataset = defineCollection({
   schema: z.object({
     title: z.string(),
     img: z.string(),
     description: z.string(),
     huggingface: z.string(),
-    }),
-  })
+  }),
+});
 
-export const collections = { dataset }
+// Definición de la colección para el overview
+const overview = defineCollection({
+  schema: z.object({
+    title: z.string(),
+
+  }),
+});
+
+// Definición de la colección para el Contributing
+const contributing = defineCollection({
+  schema: z.object({
+    title: z.string(),
+
+  }),
+});
+
+// Exporta ambas colecciones
+export const collections = { dataset, overview, contributing };
